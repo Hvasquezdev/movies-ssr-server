70db5ef21bac38a4bd21ac64dd3defc57f098a8d
Hector Vasquez <hvasquezdev@gmail.com>
2020-01-22 22:59:12 -0400

    implemented get movies, create user-movies, delete user-movies by id

--- index.js
+++ index.js
@@ -71,17 +71,68 @@ app.post('/auth/sign-up', async function (req, res, next) {
   }
 });
 
-// app.get('/movies', async function(req, res, next) {
+app.get('/movies', async function(req, res, next) {
+  try {
+    const { token } = req.cookies;
 
-// });
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/movies`,
+      headers: { 'Authorization': `Bearer ${token}` },
+      method: 'get'
+    });
 
-// app.post('/user-movies', async function(req, res, next) {
+    if (status !== 200) {
+      next(boom.badImplementation());
+    }
 
-// });
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
 
-// app.delete('/user-movies/:userMovieId', async function(req, res, next) {
+app.post('/user-movies', async function(req, res, next) {
+  try {
+    const { body: userMovie } = req;
+    const { token } = req.cookies;
 
-// });
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies`,
+      headers: { 'Authorization': `Bearer ${token}` },
+      method: 'post',
+      data: userMovie
+    });
+
+    if (status !== 201) {
+      next(boom.badImplementation());
+    }
+
+    res.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete('/user-movies/:userMovieId', async function(req, res, next) {
+  try {
+    const { userMovieId } = req.params;
+    const { token } = req.cookies;
+
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies/${userMovieId}`,
+      headers: { 'Authorization': `Bearer ${token}` },
+      method: 'delete'
+    });
+
+    if (status !== 200) {
+      next(boom.badImplementation());
+    }
+
+    res.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.listen(config.port, function () {
   console.log(`Listening on http://localhost:${config.port}`);
